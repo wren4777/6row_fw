8cccf48cd288c1bcf62a8695f1f2c491198258cb
Haru Jayasekara <vgfh.hj@gmail.com>
2020-10-10 02:06:23 +0800

    Finished?

--- 6row/keymaps/default/keymap.c
+++ 6row/keymaps/default/keymap.c
@@ -1,4 +1,4 @@
-/* Copyright 2019 COSEYFANNITUTTI
+/* Copyright 2020 wren4777
  *
  * This program is free software: you can redistribute it and/or modify
  * it under the terms of the GNU General Public License as published by
@@ -16,22 +16,25 @@
 #include QMK_KEYBOARD_H
 
 const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
-  [0] = LAYOUT_65_ansi(
-      KC_GESC, KC_1,    KC_2,    KC_3,    KC_4,   KC_5,   KC_6,   KC_7,   KC_8,   KC_9,    KC_0,    KC_MINS,  KC_EQL,  KC_BSPC,  KC_GRV,
-      KC_TAB,  KC_Q,    KC_W,    KC_E,    KC_R,   KC_T,   KC_Y,   KC_U,   KC_I,   KC_O,    KC_P,    KC_LBRC,  KC_RBRC, KC_BSLS,  KC_DEL,
-      KC_CAPS, KC_A,    KC_S,    KC_D,    KC_F,   KC_G,   KC_H,   KC_J,   KC_K,   KC_L,    KC_SCLN, KC_QUOT,  KC_ENT,            KC_PGUP,
-      KC_LSFT,          KC_Z,    KC_X,    KC_C,   KC_V,   KC_B,   KC_N,   KC_M,   KC_COMM, KC_DOT,  KC_SLSH,  KC_RSFT, KC_UP,    KC_PGDN,
-      KC_LCTL, KC_LGUI, KC_LALT,                          KC_SPC,                 KC_RALT, MO(1), KC_RCTL,  KC_LEFT, KC_DOWN,  KC_RIGHT),
+  [0] = LAYOUT_6row_ansi(
+      KC_LSFT,    KC_TAB,    KC_Y,    KC_F3,    KC_T,    KC_F7,    KC_LBRC,    KC_RBRC,                      KC_LGUI,                      KC_BSPC,                               KC_CAPS,        //Matrix border
+                             KC_N,              KC_B,              KC_SLSH,                         KC_RALT, KC_RIGHT,   KC_LEFT, KC_DOWN, KC_SPC,     KC_PGDN,                                   //Matrix border
+      KC_RSFT,    KC_Z,      KC_M,    KC_C,     KC_V,    KC_DOT,               KC_COMM,    KC_RCTL,                                        KC_ENT,     PC_PGUP,                   KC_X,           //Matrix border
+                  KC_A,      KC_J,    KC_D,     KC_F,    KC_L,     KC_SCLN,    KC_K,                                     MO(1),            KC_BSLS,    KC_MENU,                   KC_S,           //Matrix border
+                  KC_1,      KC_7,    KC_3,     KC_4,    KC_9,     KC_0,       KC_8,                         KC_F12,     KC_DEL,  KC_F11,  KC_F10,     KC_HOME,                   KC_2,           //Matrix border
+                  KC_Q,      KC_U,    KC_E,     KC_R,    KC_O,     KC_P,       KC_I,                                                                                              KC_W,           //Matrix border
+                  KC_GRAVE,  KC_6,    KC_F2,    KC_5,    KC_F8,    KC_MINS,    KC_EQL,     KC_LCTL,                               KC_INS,  KC_F9,      KC_END,                    KC_F1,          //Matrix border
+                  KC_ESC,    KC_H,    KC_F4,    KC_G,                          KC_F6,               KC_LALT,             KC_UP,            KC_F5,                                                 //Matrix border
+                                                                                                                                                                KC_BTN1, KC_BTN3,             KC_BTN2), //Matrix border
 
-  [1] = LAYOUT_65_ansi(
-     /* esc      1        2        3        4       5       6       7       8       9         0        -         =     bkspc       `~  */
-      KC_GRV,  KC_F1,   KC_F2,   KC_F3,   KC_F4,  KC_F5,  KC_F6,  KC_F7,  KC_F8,  KC_F9,   KC_F10,  KC_F11,   KC_F12,  KC_DEL,   KC_PSCR,
-     /*  tab      Q       W        E        R        T      Y        U      I        O        P        [         ]        \      delete*/ 
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, RESET,  KC_TRNS,KC_TRNS,KC_TRNS,KC_INS, KC_TRNS, KC_TRNS, KC_TRNS,  KC_PAUS, KC_TRNS,  KC_TRNS,
-     /*  caps     A       S        D        F        G      H        J      K        L        ;        '        enter             pg up*/
-      KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS, KC_TRNS, KC_TRNS,  KC_INS,            KC_HOME,
-     /* shift             Z         X        C       V       B       N      M        ,        .        /        shift     up      pg dn*/
-      KC_LSFT,          KC_TRNS, KC_TRNS, KC_TRNS,KC_TRNS,KC_TRNS,KC_TRNS,KC_MUTE, KC_TRNS, KC_TRNS, KC_TRNS,  KC_RSFT, KC_VOLU,  KC_END,
-     /* ctrl     win      alt                              space                    alt      fn       ctrl      left     down     right*/
-      KC_LCTL, KC_LGUI, KC_LALT,                          KC_TRNS,                KC_RALT, KC_TRNS, KC_RCTL,  KC_TRNS, KC_VOLD,  KC_TRNS)
+  [1] = LAYOUT_6row_ansi(
+      KC_TRNS,    KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,                      KC_TRNS,                      KC_TRNS,                               KC_TRNS,        //Matrix border
+                             KC_TRNS,           KC_TRNS,           KC_TRNS,                         KC_TRNS, KC_MNXT,    KC_MPRV, KC_MPLY, KC_TRNS,    KC_TRNS,                                   //Matrix border
+      KC_TRNS,    KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,              KC_TRNS,    KC_TRNS,                                        KC_TRNS,    KC_TRNS,                   KC_TRNS,        //Matrix border
+                  KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,                                  KC_TRNS,          KC_TRNS,    KC_TRNS,                   KC_TRNS,        //Matrix border
+                  KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,                      KC_TRNS,    KC_TRNS, KC_TRNS, KC_TRNS,    KC_BRID,                   KC_TRNS,        //Matrix border
+                  KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,                                                                                           KC_TRNS,        //Matrix border 
+                  KC_TRNS,   KC_TRNS, KC_TRNS,  KC_TRNS, KC_TRNS,  KC_TRNS,    KC_TRNS,    KC_TRNS,                               KC_SCLK, KC_TRNS,    KC_BRIU,                   KC_SYSREQ,      //Matrix border
+                  KC_PAUS,   KC_TRNS, KC_SLEP,  KC_TRNS,                       KC_PSCR,             KC_TRNS,             KC_MSTP,          KC_TRNS,                                               //Matrix border
+                                                                                                                                                                KC_TRNS, KC_TRNS,             KC_TRNS), //Matrix border
 };
